5ea5bf310839211d0c087e3c2241186ded9f201e
Gaurav Yadav <gy716828@gmail.com>
2026-04-16 15:43:44 +0530

    Add files via upload

---
+++ Python Series/D1_First_.py
@@ -0,0 +1,3 @@
+print("Hello, World!")   #Print is a function that outputs the specified message to the screen. In this case, it will display "Hello, World!" when the code is run.
+
+print("Welcome to Python programming.")  #This line will print "Welcome to Python programming." to the console.
---
+++ Python Series/D2_comment_variable.py
@@ -0,0 +1,19 @@
+#In this file we will learn about comments and variables in Python.
+#Comments are used to explain the code and make it more readable. They are ignored by the Python interpreter.
+#In Python, you can create a comment by using the hash
+#  symbol (#). Anything after the hash symbol on the same line is considered a comment.
+
+#second metthod of writing comments is by using triple quotes (''' or """). This is often used for multi-line comments or docstrings.
+
+print("Welcome to Second day of Python programming!") 
+
+#learning variables 
+#A variable is a container for storing data values. In Python, you can create a variable by assigning a value to it using the equals sign (=).
+
+name = "Gaurav"  #This line creates a variable named 'name' and assigns it the value "Gaurav". Now you can use the variable 'name' to refer to the value "Gaurav" in your code.
+age = 21  #This line creates a variable named 'age' and assigns it the value 21. Now you can use the variable 'age' to refer to the value 21 in your code.
+
+
+print("My name is",name)
+print("Welcome",name)
+print("age=",age)
